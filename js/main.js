c9f8847284a35d31d65efd0b61562bb35e026bc1
CodeMySky <codemysky@hotmail.com>
2016-01-25 15:13:59 -0500

    Site updated: 2016-01-25 15:13:59

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 require([], function (){
-
+	require.config({
+	  paths: {
+	    js: '/js',
+			fancybox: '/fancybox'
+	  },
+	});
 	var isMobileInit = false;
 	var loadMobile = function(){
-		require(['/js/mobile'], function(mobile){
+		require(['js/mobile'], function(mobile){
 			mobile.init();
 			isMobileInit = true;
 		});
 	}
 	var isPCInit = false;
 	var loadPC = function(){
-		require(['/js/pc'], function(pc){
+		require(['js/pc'], function(pc){
 			pc.init();
 			isPCInit = true;
 		});
@@ -55,7 +60,7 @@ require([], function (){
 
 	//是否使用fancybox
 	if(yiliaConfig.fancybox === true){
-		require(['/fancybox/jquery.fancybox.js'], function(pc){
+		require(['fancybox/jquery.fancybox'], function(pc){
 			var isFancy = $(".isFancy");
 			if(isFancy.length != 0){
 				var imgArr = $(".article-inner img");
@@ -72,7 +77,7 @@ require([], function (){
 	//是否开启动画
 	if(yiliaConfig.animate === true){
 
-		require(['/js/jquery.lazyload.js'], function(){
+		require(['js/jquery.lazyload'], function(){
 			//avatar
 			$(".js-avatar").attr("src", $(".js-avatar").attr("lazy-src"));
 			$(".js-avatar")[0].onload = function(){
